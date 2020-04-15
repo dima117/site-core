45f35f9e986657e89834638a2c81e2c4a34b7ab8
Dmitry Andriyanov <dima117a@yandex-team.ru>
2020-04-15 20:18:55 +0300

    new stub

---
+++ server/server.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+// Create Express server
+const app = express();
+
+// Express configuration
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/**
+ * Start Express server.
+ */
+const server = app.listen(app.get("port"), () => {
+    console.log(
+        "  App is running at http://localhost:%d in %s mode",
+        app.get("port"),
+        app.get("env")
+    );
+    console.log("  Press CTRL-C to stop\n");
+});
+
+export default server;
+
