f207774f29d9000689ce8a971ccb64e600587cce
Dmitry Andriyanov <dima117a@yandex-team.ru>
2020-04-18 21:26:57 +0300

    рендерим данные на сервере

--- server/coreMiddleware.ts
+++ server/coreMiddleware.ts
@@ -1,24 +1,37 @@
 import { Request, Response, NextFunction, RequestHandler } from 'express';
 import { parseLocation } from 'direct-react-router';
 import { createMemoryHistory } from 'history';
-import { renderToNodeStream } from 'react-dom/server';
+import { renderToString } from 'react-dom/server';
 import { createElement } from 'react';
+import serialize from 'serialize-javascript';
 
 import { buildRouterConfig } from './config';
 import { SiteConfig } from '../lib/SiteConfig';
 
+function buildDataScript(data: any): string {
+    return `<script>window.__SERVER_DATA = ${serialize(data)};</script>`;
+}
+
 export function coreMiddleware(config: SiteConfig): RequestHandler {
     const routeConfig = buildRouterConfig(config);
 
-    return (req: Request, res: Response, next: NextFunction) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
         const history = createMemoryHistory();
         history.push(req.url);
 
         const location = parseLocation(routeConfig, history.location);
-        const component = config.pages[location.key]?.component;
+        const { component, getData } = config.pages[location.key] || {};
 
         if (component) {
-            renderToNodeStream(createElement(component, null)).pipe(res);
+            const parts = [];
+
+            if (getData) {
+                parts.push(buildDataScript(await getData(location)));
+            }
+
+            parts.push(renderToString(createElement(component)))
+
+            res.send(parts.join(''));
         } else {
             next();
         }
